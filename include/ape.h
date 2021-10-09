a8e204b40dc1da7a1957372f89cd655a43c41b29
shixiongfei <xiongfei.shi@icloud.com>
2021-10-09 23:54:12 +0800

    Datetype definition

--- include/ape.h
+++ include/ape.h
@@ -13,6 +13,7 @@
 #define __APE_H__
 
 #include <stddef.h>
+#include <stdint.h>
 
 #define APE_MAJOR 0
 #define APE_MINOR 1
@@ -35,12 +36,43 @@
 extern "C" {
 #endif
 
+typedef int64_t ape_Integer;
+typedef double ape_Number;
+typedef struct ape_Object ape_Object;
 typedef struct ape_State ape_State;
+typedef ape_Object *(*ape_CFunc)(ape_State *A, ape_Object *args);
+
+typedef void (*ape_ErrorFunc)(ape_State *A, const char *errmsg, ape_Object *cl);
+typedef void (*ape_WriteFunc)(ape_State *A, void *udata, char ch);
+typedef char (*ape_ReadFunc)(ape_State *A, void *udata);
 
 typedef void *(*ape_Alloc)(void *ud, void *ptr, size_t size);
 
+typedef struct {
+  ape_ErrorFunc error;
+  ape_CFunc mark;
+  ape_CFunc gc;
+} ape_Handlers;
+
+enum {
+  APE_TPAIR,
+  APE_TFREE,
+  APE_TNIL,
+  APE_INTEGER,
+  APE_TNUMBER,
+  APE_TSYMBOL,
+  APE_TSTRING,
+  APE_TFUNC,
+  APE_TMACRO,
+  APE_TPRIM,
+  APE_TCFUNC,
+  APE_TPTR
+};
+
 APE_API ape_State *ape_newstate(ape_Alloc f, void *ud);
 APE_API void ape_close(ape_State *A);
+
+APE_API ape_Handlers *ape_handlers(ape_State *A);
 
 #ifdef __cplusplus
 };
--- src/ape.c
+++ src/ape.c
@@ -12,19 +12,101 @@
 #include "ape.h"
 #include <stdlib.h>
 
-#define ape_unused(x) ((void)x)
+enum {
+  P_DEF,
+  P_SET,
+  P_COND,
+  P_FN,
+  P_MACRO,
+  P_QUOTE,
+  P_AND,
+  P_OR,
+  P_NOT,
+  P_XOR,
+  P_DO,
+  P_CONS,
+  P_CAR,
+  P_CDR,
+  P_IS,
+  P_PRINT,
+  P_EQ,
+  P_LT,
+  P_LTE,
+  P_GT,
+  P_GTE,
+  P_ADD,
+  P_SUB,
+  P_MUL,
+  P_DIV,
+  P_MAX
+};
+
+static const char *primnames[] = {
+    "def", "set!", "cond", "fn",  "macro", "quote", "and",   "or", "not",
+    "xor", "do",   "cons", "car", "cdr",   "is?",   "print", "=",  "<",
+    "<=",  ">",    ">=",   "+",   "-",     "*",     "/"};
+
+static const char *typenames[] = {
+    "pair",   "free",     "nil",   "integer",   "number",    "symbol",
+    "string", "function", "macro", "primitive", "cfunction", "pointer"};
+
+typedef union {
+  ape_Object *o;
+  ape_CFunc f;
+  ape_Integer d;
+  ape_Number n;
+  char c;
+} Value;
+
+struct ape_Object {
+  Value car, cdr;
+};
 
 struct ape_State {
   ape_Alloc alloc;
   void *ud;
+
+  ape_Handlers handlers;
 };
 
-#define ape_realloc(A, p, n) A->alloc(A->ud, p, n)
-#define ape_malloc(A, n) ape_realloc(A, NULL, n)
-#define ape_free(A, p) ape_realloc(A, p, 0)
+#define unused(x) ((void)x)
+
+/*                Pair
+ * +---------------+---------------+
+ * |      car      |      cdr      |
+ * +---------------+---------------+
+ */
+
+#define car(x) ((x)->car.o)
+#define cdr(x) ((x)->cdr.o)
+
+/*                Tag
+ * +---+---+---+---+---+---+---+---+
+ * |          Type         |GC | 1 |
+ * +---+---+---+---+---+---+---+---+
+ * 8   7   6   5   4   3   2   1   0
+ */
+
+#define tag(x) ((x)->car.c)
+#define type(x) (tag(x) & 0x1 ? tag(x) >> 2 : APE_TPAIR)
+#define settype(x, t) (tag(x) = (t) << 2 | 1)
+
+#define isnil(x) ((x) == &nil)
+
+#define integer(x) ((x)->cdr.d)
+#define number(x) ((x)->cdr.n)
+#define prim(x) ((x)->cdr.c)
+#define cfunc(x) ((x)->cdr.f)
+#define strbuf(x) (&(x)->car.c + 1)
+
+#define STRBUFSIZE ((int)sizeof(ape_Object *) - 1)
+#define GCMARKBIT (0x2)
+#define GCSTACKSIZE (256)
+
+static ape_Object nil = {{(void *)(APE_TNIL << 2 | 1)}, {NULL}};
 
 static void *alloc_emul(void *ud, void *ptr, size_t size) {
-  ape_unused(ud);
+  unused(ud);
 
   if (size)
     return realloc(ptr, size);
@@ -32,6 +114,10 @@ static void *alloc_emul(void *ud, void *ptr, size_t size) {
   free(ptr);
   return NULL;
 }
+
+#define ape_realloc(A, p, n) A->alloc(A->ud, p, n)
+#define ape_malloc(A, n) ape_realloc(A, NULL, n)
+#define ape_free(A, p) ape_realloc(A, p, 0)
 
 ape_State *ape_newstate(ape_Alloc f, void *ud) {
   ape_Alloc alloc = f ? f : alloc_emul;
@@ -47,3 +133,5 @@ ape_State *ape_newstate(ape_Alloc f, void *ud) {
 }
 
 void ape_close(ape_State *A) { ape_free(A, A); }
+
+ape_Handlers *ape_handlers(ape_State *A) { return &A->handlers; }
