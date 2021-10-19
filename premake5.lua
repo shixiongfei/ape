f01cf53769c0b2051651ef380055af89c1fa7b0d
shixiongfei <xiongfei.shi@icloud.com>
2021-10-19 22:31:18 +0800

    Reset project premake file

--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,6 @@ solution ( "ape" )
   if _ACTION == "clean" then
     os.rmdir(".vs")
     os.rmdir("bin")
-    os.rmdir("lib")
     os.rmdir("objs")
     os.remove("ape.VC.db")
     os.remove("ape.sln")
@@ -23,74 +22,26 @@ solution ( "ape" )
     os.remove("ape.vcxproj.filters")
     os.remove("ape.vcxproj.user")
     os.remove("ape.make")
-    os.remove("main.vcxproj")
-    os.remove("main.vcxproj.filters")
-    os.remove("main.vcxproj.user")
-    os.remove("main.make")
     os.remove("Makefile")
     return
   end
 
   -- A project defines one build target
   project ( "ape" )
-  kind ( "StaticLib" )
-  language ( "C" )
-  files { "./include/*.h", "./src/*.h", "./src/*.c" }
-  includedirs { "./include" }
-  defines { "_UNICODE" }
-  staticruntime "On"
-
-  configuration ( "Release" )
-    optimize "On"
-    objdir ( "./objs" )
-    targetdir ( "./lib" )
-    defines { "NDEBUG", "_NDEBUG" }
-
-  configuration ( "Debug" )
-    symbols "On"
-    objdir ( "./objs" )
-    targetdir ( "./lib" )
-    defines { "DEBUG", "_DEBUG" }
-
-  configuration ( "vs*" )
-    defines { "WIN32", "_WIN32", "_WINDOWS",
-              "_CRT_SECURE_NO_WARNINGS", "_CRT_SECURE_NO_DEPRECATE",
-              "_CRT_NONSTDC_NO_DEPRECATE", "_WINSOCK_DEPRECATED_NO_WARNINGS" }
-
-  configuration ( "gmake" )
-    warnings  "Default" --"Extra"
-
-  configuration { "gmake", "macosx" }
-    defines { "__APPLE__", "__MACH__", "__MRC__", "macintosh" }
-
-  configuration { "gmake", "linux" }
-    defines { "__linux__" }
-
-  configuration { "gmake", "bsd" }
-    defines { "__BSD__" }
-
-  -- A project defines one build target
-  project ( "main" )
   kind ( "ConsoleApp" )
   language ( "C" )
-  targetname ("ape")
-  files { "./*.c" }
+  files { "./*.h", "./*.c" }
+  objdir ( "./objs" )
+  targetdir ( "./bin" )
   defines { "_UNICODE" }
-  includedirs { "./include" }
-  libdirs { "./lib" }
-  links { "ape" }
   staticruntime "On"
 
   configuration ( "Release" )
     optimize "On"
-    objdir ( "./objs" )
-    targetdir ( "./bin" )
     defines { "NDEBUG", "_NDEBUG" }
 
   configuration ( "Debug" )
     symbols "On"
-    objdir ( "./objs" )
-    targetdir ( "./bin" )
     defines { "DEBUG", "_DEBUG" }
 
   configuration ( "vs*" )
