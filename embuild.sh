061da8021267efd0e38eef9ed225809d8d749a6e
shixiongfei <xiongfei.shi@icloud.com>
2021-11-12 11:10:33 +0800

    Add emscripten build

---
+++ embuild.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+emcc -O2 ape.c ape_stdlib.c main.c -s WASM=1 -o ape.html --shell-file template.html
+
+if [ ! -d "./bin/" ]; then
+    mkdir ./bin
+fi
+
+mv ape.html ./bin/
+mv ape.js ./bin/
+mv ape.wasm ./bin/
+cp index.html ./bin/
