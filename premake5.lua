ac51f9ae8a626da598e0db3bdc157492a3d98949
shixiongfei <jenson.shixf@gmail.com>
2021-12-07 13:29:03 +0800

    Update premake5

--- premake5.lua
+++ premake5.lua
@@ -28,35 +28,35 @@ workspace ( "ape" )
 
   -- A project defines one build target
   project ( "ape" )
-  kind ( "ConsoleApp" )
-  language ( "C" )
-  files { "./*.h", "./*.c" }
-  objdir ( "./objs" )
-  targetdir ( "./bin" )
-  defines { "_UNICODE" }
-  staticruntime "On"
+    kind ( "ConsoleApp" )
+    language ( "C" )
+    files { "./*.h", "./*.c" }
+    objdir ( "./objs" )
+    targetdir ( "./bin" )
+    defines { "_UNICODE" }
+    staticruntime "On"
 
-  filter ( "configurations:Release" )
-    optimize "On"
-    defines { "NDEBUG", "_NDEBUG" }
+    filter ( "configurations:Release" )
+      optimize "On"
+      defines { "NDEBUG", "_NDEBUG" }
 
-  filter ( "configurations:Debug" )
-    symbols "On"
-    defines { "DEBUG", "_DEBUG" }
+    filter ( "configurations:Debug" )
+      symbols "On"
+      defines { "DEBUG", "_DEBUG" }
 
-  filter ( "action:vs*" )
-    defines { "WIN32", "_WIN32", "_WINDOWS",
-              "_CRT_SECURE_NO_WARNINGS", "_CRT_SECURE_NO_DEPRECATE",
-              "_CRT_NONSTDC_NO_DEPRECATE", "_WINSOCK_DEPRECATED_NO_WARNINGS" }
+    filter ( "action:vs*" )
+      defines { "WIN32", "_WIN32", "_WINDOWS",
+                "_CRT_SECURE_NO_WARNINGS", "_CRT_SECURE_NO_DEPRECATE",
+                "_CRT_NONSTDC_NO_DEPRECATE", "_WINSOCK_DEPRECATED_NO_WARNINGS" }
 
-  filter ( "action:gmake" )
-    warnings  "Default" --"Extra"
+    filter ( "action:gmake" )
+      warnings  "Default" --"Extra"
 
-  filter { "action:gmake", "system:macosx" }
-    defines { "__APPLE__", "__MACH__", "__MRC__", "macintosh" }
+    filter { "action:gmake", "system:macosx" }
+      defines { "__APPLE__", "__MACH__", "__MRC__", "macintosh" }
 
-  filter { "action:gmake", "system:linux" }
-    defines { "__linux__" }
+    filter { "action:gmake", "system:linux" }
+      defines { "__linux__" }
 
-  filter { "action:gmake", "system:bsd" }
-    defines { "__BSD__" }
+    filter { "action:gmake", "system:bsd" }
+      defines { "__BSD__" }
